2367a38962a1594a19567e1dbd4880cad67c287e
bjarki93 <bjarki1993@gmail.com>
2018-03-10 16:57:10 +0000

    Add files via upload

--- Sudokusolver.py
+++ Sudokusolver.py
@@ -2,7 +2,6 @@
 import tensorflow as tf
 import numpy as np
 from sklearn.model_selection import train_test_split
-from tqdm import tqdm
 
 def sudata(num_puzzles):
     #Taka inn gogn og skipta nidur i 9x9 fylki
@@ -18,49 +17,43 @@ def sudata(num_puzzles):
             sol[i, j] = s
     return quizz, sol
 #Get puzzles and solutions
-quizz, sol=sudata(num_puzzles=100)
+quizz, sol=sudata(num_puzzles=1000000)
 #quizzes = quizzes.reshape((-1, 9, 9))
 #solutions = solutions.reshape((-1,9,9))
 #Split the data
 X_train, X_test, y_train, y_test = train_test_split(quizz,sol,test_size=0.2, random_state=42)
 #Configuring the data DNN
 config = tf.contrib.learn.RunConfig(tf_random_seed=42)
+
 #feature_cols = [tf.feature_column.numeric_column("x", shape=[1, 81])]
 feature_cols = tf.contrib.learn.infer_real_valued_columns_from_input(X_train)
-dnn_clf = tf.contrib.learn.DNNClassifier(hidden_units=[300,100],
-n_classes=81,
-feature_columns=feature_cols,
-config=config)
+dnn_clf = tf.contrib.learn.DNNClassifier(hidden_units=[200,200],
+                                         n_classes=10,
+                                         feature_columns=feature_cols,
+                                         config=config)#,
+                                         #activation_fn=tf.nn.elu,
+                                         #optimizer=tf.train.MomentumOptimizer(learning_rate=learning_rate, momentum=momentum))
 dnn_clf = tf.contrib.learn.SKCompat(dnn_clf)
-clfs = [dnn_clf for i in range(81)]
 
-print('Er að fitta:')
+n=len(X_train)
+y_train1=np.zeros(n,np.int32)
 i = 0
-for clf in tqdm(clfs):
-    clf.fit(X_train, y_train[:,i], batch_size=81, max_steps=4000)
-    i=i+1
+for i in range(n):
+    y_train1[i] = y_train[i,0]
+i = 0
+dnn_clf.fit(X_train, y_train1, batch_size=500, max_steps=30000)
+
 #dnn_clf = tf.contrib.learn.SKCompat(dnn_clf)
 #dnn_clf.fit(X_train, y_train, batch_size=81, max_steps=4000)
 
-#from sklearn.metrics import accuracy_score
-#y_pred = dnn_clf.predict(X_test)
-#accuracy_score(y_test, y_pred['classes'])
-
-sud2pred = np.zeros((1, 81), np.int32)
-
-Sud2 = X_test[:1]
-Sud2sol = y_test[:1]
-
+from sklearn.metrics import accuracy_score
+m=len(y_test)
+y_pred = dnn_clf.predict(X_test)
+y_pred=y_pred['classes']
+y_test1=np.zeros(m,np.int32)
 i = 0
-for clf in clfs:
-    if Sud2[0,i] == 0:  
-        sud2pred[0,i] = clf.predict(sum(Sud2))
-    else:
-        sud2pred[0,i] = Sud2[0,i]
-    i=i+1
+for i in range(m):
+    y_test1[i] = y_test[i,0]
 
-test=sud2pred==Sud2sol
-number=np.sum(test)
-m,n=test.shape
-accuracy=100*number/(m*n)
-print("Nakvaemni: %.4f" % accuracy + '%')
+accuracy_score=accuracy_score(y_test1, y_pred)
+print(accuracy_score)
